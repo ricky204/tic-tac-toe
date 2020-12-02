3739a02081198d240e6e81abc352ad401b494e3f
ricky204 <75343389+ricky204@users.noreply.github.com>
2020-12-01 16:14:55 -0800

    Add files via upload

---
+++ Tic-Tac-Toe/js/GameView.js
@@ -0,0 +1,19 @@
+// This contains all of the logic for rendering out of the view & reacting to updates
+export default class GameView {
+    constructor(root) {
+        this.root = root;
+        this.root.innerHTML = `
+            <div class="header">
+                <div class="header__turn">
+                    X's turn
+                </div>
+                <div class="header__status">
+                    In Progress
+                </div>
+                <button type="button" class="header__restart">
+                    <i class="material-icons">refresh</i>
+                </button>
+            </div>
+        `;
+    }
+}
